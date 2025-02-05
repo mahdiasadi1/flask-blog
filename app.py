a74e25af09428d312dcbbb1396805b7c7372f8b4
mahdi <asadi.mahdi@gmail.com>
2025-02-06 02:33:57 +0330

    add mod_admin

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
+from mod_admin import admin
+app.register_blueprint(admin)
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ mod_admin/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+admin =  Blueprint('admin',__name__,url_prefix='/admin')
+@admin.route('/')
+def admin_index():
+    return "hello admin "
+    
