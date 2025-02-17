1fa0bb146089c1d02d256ef2b660e123b7d109b5
mahdi <asadi.mahdi@gmail.com>
2025-02-18 03:03:53 +0330

    add many to many relationship to category and posts  model

--- mod_blog/models.py
+++ mod_blog/models.py
@@ -11,4 +11,10 @@ class Category(db.Model):
     id=db.Column(db.Integer,primary_key=True,unique=True)
     name=db.Column(db.String(256))
     description=db.Column(db.String(256))
-    slug=db.Column(db.String(256))    
+    slug=db.Column(db.String(256))  
+    posts = db.relationship('Post', secondary='category_post', backref=db.backref('categories', lazy='dynamic'))
+
+category_post = db.Table('category_post',
+    db.Column('post_id', db.Integer, db.ForeignKey('posts.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('categories.id'), primary_key=True)
+                         )      
